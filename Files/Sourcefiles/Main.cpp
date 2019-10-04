9614d799feea9bc4b9d0e8d210fe6055daa322a8
Lene <you@example.com>
2019-10-04 10:36:40 +0200

    Added main

--- Files/Sourcefiles/Main.cpp
+++ Files/Sourcefiles/Main.cpp
@@ -1,1 +1,15 @@
+#include "MinesweeperWindow.h"
 
+int main()
+{
+	srand(static_cast<unsigned int>(time(nullptr)));
+	Fl::background(200, 200, 200);
+	constexpr int width = 20;
+	constexpr int height = 17;
+	constexpr int mines = 15;
+
+	Point startPoint{ 200,250 };
+	MinesweeperWindow mw{ startPoint, width, height, mines, "Lene's Minesweeper ;)" };
+	return gui_main();
+
+}
